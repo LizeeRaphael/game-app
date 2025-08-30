c14f3dbbd12583fa95cd69d91365349f04a614b2
LizeeRaphael <lizeeraphael@gmail.com>
2025-08-30 20:51:25 +0100

    fixed bug on boostrap installation to work properly

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
-import Message from "./Message";
+// import Message from "./Message";
+
+// function App() {
+//   return <div><Message /></div>;
+// }
+// export default App;
 
 function App() {
-  return <div><Message /></div>;
+  return (
+    <div className="container text-center mt-5">
+      <h1 className="text-primary">Hello Bootstrap 🎉</h1>
+      <button className="btn btn-success">Click Me</button>
+    </div>
+  );
 }
+
 export default App;
