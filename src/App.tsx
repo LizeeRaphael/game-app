bfd2e8debaa0fb9078c151a6853075da4038ed36
LizeeRaphael <lizeeraphael@gmail.com>
2025-09-01 19:33:30 +0100

    added item key

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,35 @@
-import ListGroup from "./components/ListGroup";
+// import ListGroup from "./components/ListGroup";
 
-function App() {
+// function App() {
+//   return (
+//     <div>
+//       <h1>List</h1>
+//       <ul className="list-group">
+//         <li className="list-group-item">An item</li>
+//         <li className="list-group-item">A second item</li>
+//         <li className="list-group-item">A third item</li>
+//         <li className="list-group-item">A fourth item</li>
+//         <li className="list-group-item">And a fifth one</li>
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+function ListGroup() {
+  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
   return (
-    <div>
+    <>
       <h1>List</h1>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
       </ul>
-    </div>
+    </>
   );
 }
 
-export default App;
+export default ListGroup;
