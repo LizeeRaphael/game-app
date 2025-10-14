9eabb5f8c50b78a69b54c68098c0f6f5db9b6063
LizeeRaphael <lizeeraphael@gmail.com>
2025-10-14 15:54:07 +0100

    Rebuilt index and relinked project

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,46 @@
+// // import ListGroup from "./components/ListGroup";
+
+// // function App() {
+// //   return (
+// //     <div>
+// //       <h1>List</h1>
+// //       <ul className="list-group">
+// //         <li className="list-group-item">An item</li>
+// //         <li className="list-group-item">A second item</li>
+// //         <li className="list-group-item">A third item</li>
+// //         <li className="list-group-item">A fourth item</li>
+// //         <li className="list-group-item">And a fifth one</li>
+// //       </ul>
+// //     </div>
+// //   );
+// // }
+
+// // export default App;
+
+// function ListGroup() {
+//   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+//   return (
+//     <>
+//       <h1>List</h1>
+//       {items.length === 0 && <p>No item found.</p>}
+//       <ul className="list-group">
+//         {items.map((item) => (
+//           <li
+//             className="list-group-item"
+//             key={item}
+//             onClick={() => console.log("Clicked")}
+//           >
+//             {item}
+//           </li>
+//         ))}
+//       </ul>
+//     </>
+//   );
+// }
+
+// export default ListGroup;
+
 // import ListGroup from "./components/ListGroup";
 
 // function App() {
@@ -17,6 +60,10 @@
 
 // export default App;
 
+
+
+// handling event
+
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
@@ -29,7 +76,7 @@ function ListGroup() {
           <li
             className="list-group-item"
             key={item}
-            onClick={() => console.log("Clicked")}
+            onClick={() => console.log(item)}
           >
             {item}
           </li>
@@ -40,3 +87,4 @@ function ListGroup() {
 }
 
 export default ListGroup;
+
