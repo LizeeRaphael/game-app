d22af1d1611b4a60e21acad75ea4b113a84d9f7f
LizeeRaphael <lizeeraphael@gmail.com>
2025-10-16 12:20:03 +0100

    managed first indexing state

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,12 @@
 // // import ListGroup from "./components/ListGroup";
 
-// // function App() {
-// //   return (
-// //     <div>
-// //       <h1>List</h1>
-// //       <ul className="list-group">
-// //         <li className="list-group-item">An item</li>
-// //         <li className="list-group-item">A second item</li>
-// //         <li className="list-group-item">A third item</li>
-// //         <li className="list-group-item">A fourth item</li>
-// //         <li className="list-group-item">And a fifth one</li>
-// //       </ul>
-// //     </div>
-// //   );
-// // }
-
-// // export default App;
-
-// function ListGroup() {
-//   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-
-//   return (
-//     <>
-//       <h1>List</h1>
-//       {items.length === 0 && <p>No item found.</p>}
-//       <ul className="list-group">
-//         {items.map((item) => (
-//           <li
-//             className="list-group-item"
-//             key={item}
-//             onClick={() => console.log("Clicked")}
-//           >
-//             {item}
-//           </li>
-//         ))}
-//       </ul>
-//     </>
-//   );
-// }
-
-// export default ListGroup;
-
-// import ListGroup from "./components/ListGroup";
-
-// function App() {
-//   return (
-//     <div>
-//       <h1>List</h1>
-//       <ul className="list-group">
-//         <li className="list-group-item">An item</li>
-//         <li className="list-group-item">A second item</li>
-//         <li className="list-group-item">A third item</li>
-//         <li className="list-group-item">A fourth item</li>
-//         <li className="list-group-item">And a fifth one</li>
-//       </ul>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-// handling event
-
-import { MouseEvent } from "react";
+import { useState } from "react";
 
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
-  //Event handler
-  const handleClick = (event: MouseEvent) => console.log(event);
+  //Hook allows us tap into built-in functions in React
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
@@ -76,7 +14,15 @@ function ListGroup() {
       {items.length === 0 && <p>No item found.</p>}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+          <li
+            className={
+              selectedIndex === index ? "list-group-item active" : "list-group"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
             {item}
           </li>
         ))}
