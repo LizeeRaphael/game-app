05eafdf1eb0a3acebe4d8d2bfc7a70451b515853
LizeeRaphael <lizeeraphael@gmail.com>
2025-10-23 16:58:08 +0100

    added button in replacement

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,52 @@
-// // import ListGroup from "./components/ListGroup";
+// // // import ListGroup from "./components/ListGroup";
 
-import { useState } from "react";
+// import { useState } from "react";
 
-// { items:[], heading: strings }
-interface Props {
-  items: string[];
-  heading: string;
-}
+// // { items:[], heading: strings }
+// interface Props {
+//   items: string[];
+//   heading: string;
+// }
 
-function ListGroup(props: Props) {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+// function ListGroup(props: Props) {
+//   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
-  //Hook allows us tap into built-in functions in React
-  const [selectedIndex, setSelectedIndex] = useState(-1);
+//   //Hook allows us tap into built-in functions in React
+//   const [selectedIndex, setSelectedIndex] = useState(-1);
 
+//   return (
+//     <>
+//       <h1>List</h1>
+//       {items.length === 0 && <p>No item found.</p>}
+//       <ul className="list-group">
+//         {items.map((item, index) => (
+//           <li
+//             className={
+//               selectedIndex === index ? "list-group-item active" : "list-group"
+//             }
+//             key={item}
+//             onClick={() => {
+//               setSelectedIndex(index);
+//             }}
+//           >
+//             {item}
+//           </li>
+//         ))}
+//       </ul>
+//     </>
+//   );
+// }
+
+// export default ListGroup;
+
+import Button from "./components/Button";
+
+function App() {
   return (
-    <>
-      <h1>List</h1>
-      {items.length === 0 && <p>No item found.</p>}
-      <ul className="list-group">
-        {items.map((item, index) => (
-          <li
-            className={
-              selectedIndex === index ? "list-group-item active" : "list-group"
-            }
-            key={item}
-            onClick={() => {
-              setSelectedIndex(index);
-            }}
-          >
-            {item}
-          </li>
-        ))}
-      </ul>
-    </>
+    <div>
+      <button> </button>
+    </div>
   );
 }
 
-export default ListGroup;
+export default App;
